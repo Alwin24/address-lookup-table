915f656d1c073cc73a84a03383820812db6ab9c2
alwin <alwin24012002@gmail.com>
2023-02-19 20:34:15 +0530

    .

--- index.js
+++ index.js
@@ -47,12 +47,11 @@ const [lookupTableInst, lookupTableAddress] = AddressLookupTableProgram.createLo
 	payer: payer.publicKey,
 	recentSlot: slot,
 })
-// const lookupTableAddress = new PublicKey("8Zr8ymaWMztnt51bQ9YRrx8QBCuEx5FJwWjoe7F5x6g5")
 
-console.log('lookup table address:', lookupTableAddress.toBase58())
+// console.log('lookup table address:', lookupTableAddress.toBase58())
 
 let addresses = [
-    // elixir
+	// elixir
 	'Sysvar1nstructions1111111111111111111111111',
 	'E1XRkj9fPF2NQUdoq41AHPqwMDHykYfn5PzBXAyDs7Be',
 	'ComputeBudget111111111111111111111111111111',
@@ -63,7 +62,7 @@ let addresses = [
 	'A6bixUiWZVQvg4odnWkXfPxoyVhhqn1kGdc8fRU1brXN',
 	'3zXeaxfj5sEQ1ywxtMutqTA7WaEkFbZa26r9bzTvsSvW',
 	'49tfqB49v34F6oyQZhuBTwunPGpTPi6UdCP8CziTJxFH',
-    // raydium
+	// raydium
 	'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
 	'SysvarRent111111111111111111111111111111111',
 	'srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX',
@@ -83,24 +82,71 @@ let addresses = [
 	'3Y4xMRBdJzgnnQGK2SdzNxY5dxbbiJX2KZCRbEGoEa1g',
 	'31LB8ZykVSqYpvGQiwXGHf75Rpmd3TbTprsyYJkeGUZm',
 	'2QedB5jfRe3EeiEUFsRXCYDrYBFLPiTauQiEZAyp8nJp',
+
+	'whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc',
+	'675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8',
+	'11111111111111111111111111111111',
+	'SysvarC1ock11111111111111111111111111111111',
+	'SysvarRent111111111111111111111111111111111',
+	'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+	'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+	'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+	'2qGyiNeWyZxNdkvWHc2jT5qkCnYa1j1gDLSSUmyoWMh8',
+	'E1XRkj9fPF2NQUdoq41AHPqwMDHykYfn5PzBXAyDs7Be',
+	'fee6uQpfQYhfZUxiYLvpAjuCGNE7NTJrCoXV8tsqsn6',
+	'So11111111111111111111111111111111111111112',
+	'6kLLewcYCvUK6xLQE1ep36ReamuTLFuTWwhCnbMCb3pd',
+
+	'srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX',
+	'5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1',
+	'9wTfFctuYVojD7MB2AQDUQmTA3pkZYev6GmZje9QfGQS',
+	'BQXDYWZdtXqeLXFWYeRhLrGh8gcTmDQZQc92ENMaXSry',
+	'Et3d3Jcxe2rZM5J5es9dEfSm1gKGeQoovfzcSRUeJicZ',
+	'Bg2isp4C176o5615BLWjNvfModGCQFYyXtMP3zgPDCrR',
+	'93L67quQSxUNtbYroxk46PYaoqyps4h3yw2BkwWRyU7F',
+	'DMMBTBwEjwwaV5dSSPVYWKopatHg2MUh8R6mUqGWNydw',
+	'413x5fBqSZyT4DyS9oauPvp1cbMVLhnzeHGe4fUmd8yV',
+	'E5UaBddUS44BNyYiK12h5WQ5y9TrrwnQPD5uVrFSGixb',
+	'25Cuzmbjgi3koqFaxn6QzRizLiYyNB7NtN1tdZShUA5q',
+	'EVseAB3KCY8qmXiUQH2rDdGbRZoAdCY1URUshrPWDcHh',
+	'QsrLS5YGvF574XyhDNyS3g22NhSpqTwW6zmzfJjVMN8',
+	'8SYnBCKGYM12ozjiXqYDbSTe4atQTk1y2sXMCpUt7NGn',
+	'7YqhZ8W445LjpEymK2Ta2NvxJr1NRhxowotVybvZzFYe',
+	'H4bCvPGnDNzuGjF3H7uuqUWK1skTpBRcPjkeeVn4UZ2b',
+	'AB3EayWSFZAU5jCEw64spXYCjPnS7TH3LW8im62oSKNZ',
+	'3mRTPVDoZzGzsbA9E1eU6va7wYELu5nQTD6aD5ar4AJb',
+	'3UZ2wVm2PuJSUcEvL6vunAovfj1CDn3wNGiEvojgTLQw',
+	'jLF923FvHFbzGhiNvtQVxuFrz7q6bMzqDhGF5Yb9w2q',
 ]
+
+// let lookupTableAccount = await connection
+// 	.getAddressLookupTable(new PublicKey('14SU2WQHMXzjZFtxUpt4hnonYz7DbywWnWuHc6h3fL8U'))
+// 	.then((res) => res.value)
+
+// addresses.concat(lookupTableAccount.state.addresses.map((acc) => acc.toBase58()))
+
+// lookupTableAccount = await connection
+// 	.getAddressLookupTable(new PublicKey('4oA28x6ZA1sNPvXLWLG7aNcoPoNj4a6F3QYPyTS2HvYE'))
+// 	.then((res) => res.value)
+
+// addresses.concat(lookupTableAccount.state.addresses.map((acc) => acc.toBase58()))
+
+// lookupTableAccount = await connection
+// 	.getAddressLookupTable(new PublicKey('9oNDFSLwMJKd4GcXMwDKkrt5bg5nqQzUJTRkn8F8zqfN'))
+// 	.then((res) => res.value)
 
 const extendInstruction = AddressLookupTableProgram.extendLookupTable({
 	payer: payer.publicKey,
 	authority: payer.publicKey,
 	lookupTable: lookupTableAddress,
-	addresses: addresses.map((address) => new PublicKey(address)),
+	addresses: addresses.map((acc) => new PublicKey(acc)),
 })
 
 await sendTransactionV0(connection, [lookupTableInst, extendInstruction], payer)
 
-// const lookupTableAccount = await connection
-//     .getAddressLookupTable(new PublicKey("Bvbui5svPefbMvoH37ANbQKM2fdU6YbbDFeiHmY9cpRs"))
-//     .then((res) => res.value);
+console.log('Table address from cluster:', lookupTableAccount.key.toBase58())
 
-// console.log("Table address from cluster:", lookupTableAccount.key.toBase58());
-
-// for (let i = 0; i < lookupTableAccount.state.addresses.length; i++) {
-//     const address = lookupTableAccount.state.addresses[i];
-//     console.log(i, address.toBase58());
-// }
+for (let i = 0; i < lookupTableAccount.state.addresses.length; i++) {
+	const address = lookupTableAccount.state.addresses[i]
+	console.log(i, address.toBase58())
+}
